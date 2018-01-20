694687ea8267aadf582ebc9a55e1276c358a7345
Cherie Lum <charismatic.cherie@gmail.com>
2018-01-20 10:31:48 -0500

    adding exercise

--- index.js
+++ index.js
@@ -117,3 +117,24 @@ var isActive = true;
     }
     
 
+    //exercise Ice Cream (3-6)
+//     * Write a single if statement that logs out the message:
+//  * 
+//  * "I'd like two scoops of __________ ice cream in a __________ with __________."
+//  * 
+//  * ...only if:
+//  *   - flavor is "vanilla" or "chocolate"
+//  *   - vessel is "cone" or "bowl"
+//  *   - toppings is "sprinkles" or "peanuts"
+
+var flavor = "strawberry";
+var vessel = "cone";
+var toppings = "cookies";
+
+// Add your code here
+if ((flavor === "vanilla" || flavor === "chocolate") && 
+    (vessel === "cone" || vessel === "bowl") && 
+    (toppings === "sprinkles" || toppings === "peanuts")) {
+    console.log("I'd like two scoops of " +flavor+ " ice cream in a "+vessel+" with "+toppings+".");
+    } 
+
