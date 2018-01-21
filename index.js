70f39bb1aaf8bcfaf3f91266f5fdb691b8507fb0
Cherie Lum <charismatic.cherie@gmail.com>
2018-01-21 14:42:43 -0500

    adding exercise

--- index.js
+++ index.js
@@ -159,3 +159,34 @@ if ((shirtWidth <20) && (shirtLength < 29 ) && (shirtSleeve < 8.38)){
     console.log("3XL");
 } else 
     console.log("N/A");
+
+
+/*Ternary Operators */
+var adult = true;
+var preorder = true;
+
+console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "the gate") + ".");
+/*this means that if adult is true, ticket is 40, else it is $20 and if pre-order is true, you pick up at will call, otherwise, you pick up at gate */
+
+
+// * Programming Quiz - Navigating the Food Chain (3-8)
+//  *
+//  * Use a series of ternary operator to set the category to one of the following:
+//  *   - "herbivore" if an animal eats plants
+//  *   - "carnivore" if an animal eats animals
+//  *   - "omnivore" if an animal eats plants and animals
+//  *   - undefined if an animal doesn't eat plants or animals
+//  *
+//  * Notes
+//  *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+//  *   - `if` statements aren't allowed ;-)
+//  */
+
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = true;
+var eatsAnimals = true;
+
+var category = (eatsPlants && eatsAnimals ? "omnivore" : eatsPlants ? "herbivore" : eatsAnimals? "carnivore" : undefined );
+      
+       
+console.log(category);
